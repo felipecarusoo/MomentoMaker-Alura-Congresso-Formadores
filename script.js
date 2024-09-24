e0d67146782bfed347984c5d3a60fc532b29b45c
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-09-24 16:31:54 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 function exibirAlerta() {
-    const mensagemSecreta = "Sua mensagem secreta aqui";
+
+    const mensagemSecreta = "A bibliotecaria com medo de perder o emprego para novas tecnologias, apagou o arquivo do computador, mas derrabou milkshake deixando a maior bagunca ao termino do expediente. Seu amante Arthur, conivente da historia, entrou na biblioteca para limpar e eliminar vestigios, nao conseguindo em 100%.";
     const elemento = document.querySelector(".texto");
     elemento.classList.add("texto-secreto");
     elemento.innerHTML = mensagemSecreta;
