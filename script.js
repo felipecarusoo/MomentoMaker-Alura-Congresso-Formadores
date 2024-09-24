4e92497180afc40d56d44e7174fffe692acb32e2
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-09-24 16:38:45 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function exibirAlerta() {
 
-    const mensagemSecreta = "A bibliotecaria com medo de perder o emprego para novas tecnologias, apagou o arquivo do computador, mas derrabou milkshake deixando a maior bagunca ao termino do expediente. Seu amante Arthur, conivente da historia, entrou na biblioteca para limpar e eliminar vestigios, nao conseguindo em 100%.";
+    const mensagemSecreta = "mas esquece a presenca das cameras e precisa entrar em contato com o prof de programacao que coloca as cameras em loooping e ninguém consegue verificar os acessos. Até que a investigadora, com um instrumento ultramegasofisticado, descobre o romance entre os amantes e revela tudo ao diretor traído.";
     const elemento = document.querySelector(".texto");
     elemento.classList.add("texto-secreto");
     elemento.innerHTML = mensagemSecreta;
